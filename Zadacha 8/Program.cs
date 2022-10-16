b3234c7f0b72b7f3a49e47e2136194e255081f8a
ElenaRudakova <er171173@gmail.com>
2022-10-16 23:11:10 +0300

    Задача 8

---
+++ Zadacha 8/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int namber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваши четные числа");
+int i = 1;
+while (i <= namber)
+{
+    if (i % 2 == 0)
+Console.WriteLine ("{0}",i);i++;
+
+}
